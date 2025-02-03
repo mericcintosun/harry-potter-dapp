c56147650d5c316248f52a7b974f49aea9e76983
Alp <baturalperbay@gmail.com>
2025-02-03 16:35:51 +0300

    kontrat isimlerini değiştirdim ve auction ve NFT'yi ikiye parçalıyorum.

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+//import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFTAuction is ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
+
+//    function mint(address to, string memory tokenURI) public onlyOwner {
+//        uint256 tokenId = _tokenIdCounter.current();
+//        _safeMint(to, tokenId);
+//        _setTokenURI(tokenId, tokenURI);
+//        _tokenIdCounter.increment();
+//    }
+
+// Counters.sol kaldırılmış, onun yerine yeni önerilene değiştirdim.
+// https://github.com/OpenZeppelin/openzeppelin-contracts/issues/4233#issuecomment-1561791911
+
+    function mint(address receiver) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter;
+        _safeMint(receiver, tokenId);
+        _tokenIdCounter += 1;
+    }
+
+    // Toplam NFT sayısını döndürme
+    function totalSupply() public view returns (uint256) {
+        return _tokenIdCounter;
+    }
+}
