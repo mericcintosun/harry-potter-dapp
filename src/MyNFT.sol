71cc01e7eeaef8a8eef1e61790c85ea075232000
Alp <baturalperbay@gmail.com>
2025-02-04 16:56:03 +0300

    foundry deneme v2

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 //import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFTAuction is ERC721URIStorage, Ownable {
+contract NFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
     constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
 
