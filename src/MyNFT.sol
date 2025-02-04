360643108b769bad2e4b643989ca9984a8682347
Alp <baturalperbay@gmail.com>
2025-02-05 02:16:07 +0300

    The tests for MyNFT; temporarily removing URIStorage from the MyNFT:NFT's inherited classes before the implementation.

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-//import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721Burnable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract NFT is ERC721URIStorage, Ownable {
+contract NFT is ERC721Burnable, Ownable {
     uint256 private _tokenIdCounter;
 
     // birden çok collection olma ihtimaline karşı dinamik sembol || eski versiyon: "MyNFT", "MNFT"
