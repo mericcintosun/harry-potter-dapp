e83a3482365ba25b81cd5d54faaaf6d7ff680f00
Alp <baturalperbay@gmail.com>
2025-02-06 17:43:31 +0300

    Added a general contract test scenario to the Auction test file.

--- test/Auction.t.sol
+++ test/Auction.t.sol
@@ -21,7 +21,6 @@ contract TestAuction is Test {
         vm.deal(ALICE, 50 ether);
     }
 
-
     /*
     function test_onlyOwnerModifier() public {
         vm.warp(block.timestamp + 6 days);
@@ -30,7 +29,6 @@ contract TestAuction is Test {
         auction.withdraw();
     }
     */
-
 
     function test_auctionEnded() public {
         vm.warp(block.timestamp + 5 days + 1 seconds);
@@ -60,7 +58,6 @@ contract TestAuction is Test {
         assertEq(BOB.balance, 50 ether);
         assertEq(auction.getContractBalance(), 15 ether);
     }
-
 
     function test_Bidding() public {
         vm.prank(BOB);
@@ -101,4 +98,88 @@ contract TestAuction is Test {
         //check if variable ended was set to true as it should be.
         assertEq(auction.ended(), true);
     }
+
+    //https://blog.finxter.com/solidity-by-example-part-12-simple-open-auction/
+    //doing the test scenario from here.
+    function test_scenarioFirst() public {
+        address payable BENEFICIARY_TEST_SCENARIO_1 = payable(address(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4));
+        address ACCOUNT_01_TEST_SCENARIO_1 = address(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+        address ACCOUNT_02_TEST_SCENARIO_1 = address(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+        address ACCOUNT_03_TEST_SCENARIO_1 = address(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
+        address ACCOUNT_04_TEST_SCENARIO_1 = address(0x617F2E2fD72FD9D5503197092aC168c91465E7f2);
+
+        auction = new Auction(240, BENEFICIARY_TEST_SCENARIO_1);
+
+        vm.deal(ACCOUNT_01_TEST_SCENARIO_1, 100 wei);
+        vm.deal(ACCOUNT_02_TEST_SCENARIO_1, 100 wei);
+        vm.deal(ACCOUNT_03_TEST_SCENARIO_1, 100 wei);
+        vm.deal(ACCOUNT_04_TEST_SCENARIO_1, 100 wei);
+        uint initialBalance_BENEFICIARY_TEST_SCENARIO_1 = address(BENEFICIARY_TEST_SCENARIO_1).balance;
+
+        //Account 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 bids 10 Wei;
+        vm.prank(ACCOUNT_01_TEST_SCENARIO_1);
+        auction.bid{value: 10 wei}();
+
+        //Account 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db bids 25 Wei;
+        vm.prank(ACCOUNT_02_TEST_SCENARIO_1);
+        auction.bid{value: 25 wei}();
+
+        //Account 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB bids 25 Wei (rejected);
+        vm.prank(ACCOUNT_03_TEST_SCENARIO_1);
+        vm.expectRevert(abi.encodeWithSelector(Auction.BidNotHighEnough.selector, 25 wei));
+        auction.bid{value: 25 wei}();
+
+        //Account 0x617F2E2fD72FD9D5503197092aC168c91465E7f2 bids 35 Wei;
+        vm.prank(ACCOUNT_04_TEST_SCENARIO_1);
+        auction.bid{value: 35 wei}();
+
+        //Account 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 bids 40 Wei + initiates premature auction end;
+        vm.prank(ACCOUNT_01_TEST_SCENARIO_1);
+        auction.bid{value: 40 wei}();
+
+        //premature end, should fail.
+        vm.warp(block.timestamp + 120 seconds);
+        vm.prank(ACCOUNT_01_TEST_SCENARIO_1);
+        vm.expectRevert(Auction.AuctionNotYetEnded.selector);
+        auction.auctionEnd();
+
+        //Account 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2 withdraws his bids;
+        vm.prank(ACCOUNT_01_TEST_SCENARIO_1);
+        auction.withdraw();
+
+        //Account 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db withdraws his bids;
+        vm.prank(ACCOUNT_02_TEST_SCENARIO_1);
+        auction.withdraw();
+
+        //Account 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB withdraws his bids;
+        vm.prank(ACCOUNT_03_TEST_SCENARIO_1);
+        auction.withdraw();
+
+        //Account 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB initiates timely auction end;
+        vm.warp(block.timestamp + 241 seconds);
+        vm.prank(ACCOUNT_03_TEST_SCENARIO_1);
+        auction.auctionEnd();
+
+        //Account 0x617F2E2fD72FD9D5503197092aC168c91465E7f2 withdraws his bids;
+        vm.prank(ACCOUNT_04_TEST_SCENARIO_1);
+        auction.withdraw();
+
+
+                                       // *** BALANCE CHECKS *** //
+        //at the end account 2,3,4 should all have the starting point of 100, while account 1 will have 60.
+        //beneficiary should be up 40 compared to the starting point.
+
+        //account 2,3,4
+        assertEq(ACCOUNT_02_TEST_SCENARIO_1.balance, 100 wei);
+        assertEq(ACCOUNT_03_TEST_SCENARIO_1.balance, 100 wei);
+        assertEq(ACCOUNT_04_TEST_SCENARIO_1.balance, 100 wei);
+
+        //account 1
+        assertEq(ACCOUNT_01_TEST_SCENARIO_1.balance, 60 wei);
+
+        //beneficiary
+        uint endBalance_BENEFICIARY_TEST_SCENARIO_1 = address(BENEFICIARY_TEST_SCENARIO_1).balance;
+        assertEq(initialBalance_BENEFICIARY_TEST_SCENARIO_1 + 40 wei, endBalance_BENEFICIARY_TEST_SCENARIO_1);
+    }
 }
+
